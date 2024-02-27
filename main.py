c2715a92652c2f6166ae677b568a5343a5ed694e
olehandro <odpash.itmo@gmail.com>
2024-02-27 17:46:32 +0300

    updated dockerfile

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import aiohttp_cors
+
 from src.router import web, app
 
 if __name__ == '__main__':
+    cors = aiohttp_cors.setup(app)
+    for route in list(app.router.routes()):
+        cors.add(route, {
+            "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True,
+                expose_headers="*",
+                allow_headers="*",
+            )
+        })
+
     web.run_app(app, host='0.0.0.0', port=5003)
