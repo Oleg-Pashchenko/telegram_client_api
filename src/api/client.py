3eee0db35721b63a06dfcf7cd53a4d9e45513700
olehandro <odpash.itmo@gmail.com>
2024-02-29 11:44:27 +0300

    updated dockerfile

--- src/api/client.py
+++ src/api/client.py
@@ -50,10 +50,7 @@ async def try_auth_with_2fa(data: AuthWith2FAData):
 
 async def get_updates(data: GetUpdatesData):
     tg: Tg = get_tg_entity(data.session_name)
-    if tg.secret_password:
-        await tg.client.start(phone=tg.phone, password=tg.secret_password)
-    else:
-        await tg.client.start(phone=tg.phone)
+    await tg.client.connect()
     response = await tg.get_updates()
     save_tg_entity(tg)
     await tg.client.disconnect()
